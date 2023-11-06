5b3516ca59b4328488bd940d94e7ff69a7516cb1
Nevralgie <93102912+Nevralgie@users.noreply.github.com>
2023-11-06 16:05:19 +0100

    Create app.py

---
+++ app.py
@@ -0,0 +1,30 @@
+from flask import Flask, request, render_template
+import os
+
+app = Flask(__name__)
+
+# Define the upload folder
+UPLOAD_FOLDER = 'uploads'
+app.config['UPLOAD_FOLDER'] = UPLOAD_FOLDER
+
+@app.route('/')
+def index():
+    return render_template('index.html')
+
+@app.route('/upload', methods=['POST'])
+def upload_file():
+    if 'file' not in request.files:
+        return 'No file part'
+
+    file = request.files['file']
+
+    if file.filename == '':
+        return 'No selected file'
+
+    if file:
+        filename = file.filename
+        file.save(os.path.join(app.config['UPLOAD_FOLDER'], filename))
+        return 'File successfully uploaded'
+
+if __name__ == '__main__':
+    app.run(debug=True)
