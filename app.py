48546874d401b650278313d6f0ede36b661f6347
Nevralgie <93102912+Nevralgie@users.noreply.github.com>
2023-11-07 10:43:15 +0100

    Update app.py

--- app.py
+++ app.py
@@ -1,19 +1,30 @@
-from flask import Flask, render_template, request
-from werkzeug import secure_filename
-app = Flask(__name__)
+from flask import Flask, request, render_template
+import os
 
-@app.route('/upload')
+app = Flask(__name)
+
+# Define the upload folder
+UPLOAD_FOLDER = 'uploads'
+app.config['UPLOAD_FOLDER'] = UPLOAD_FOLDER
+
+@app.route('/')
+def index():
+    return render_template('index.html')
+
+@app.route('/upload', methods=['POST'])
 def upload_file():
-   return render_template('upload.html')
-	
-@app.route('/uploader', methods = ['GET', 'POST'])
-def upload_file():
-   if request.method == 'POST':
-      f = request.files['file']
-      f.save(secure_filename(f.filename))
-      return 'file uploaded successfully'
-		
+    if 'file' not in request.files:
+        return 'No file part'
+
+    file = request.files['file']
+
+    if file.filename == '':
+        return 'No selected file'
+
+    if file:
+        filename = file.filename
+        file.save(os.path.join(app.config['UPLOAD_FOLDER'], filename))
+        return 'File successfully uploaded'
+
 if __name__ == '__main__':
-   app.run(debug = True)
-
-#end
+    app.run(debug=True)
