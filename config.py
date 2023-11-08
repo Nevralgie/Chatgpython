26beb0b2278ceeda3d8ed90e5409d9040fbb3239
Nevralgie <93102912+Nevralgie@users.noreply.github.com>
2023-11-08 16:11:30 +0100

    Delete config.py

--- config.py
+++
@@ -1,6 +0,0 @@
-ACCOUNT_NAME = "trenstoragetrain"
-ACCOUNT_KEY = "Ejuv1L7PhgAzydfcDietIOv8dBejza1kuXqprTp/wycOeZcHnJlXCZNWOmPA/JCxFoqGiblRag6x+ASttCA8aw=="
-CONNECTION_STRING = "DefaultEndpointsProtocol=https;AccountName=trenstoragetrain;AccountKey=Ejuv1L7PhgAzydfcDietIOv8dBejza1kuXqprTp/wycOeZcHnJlXCZNWOmPA/JCxFoqGiblRag6x+ASttCA8aw==;EndpointSuffix=core.windows.net"
-CONTAINER = "test104"
-ALLOWED_EXTENSIONS = set(['txt', 'pdf', 'png', 'jpg', 'jpeg'])
-MAX_CONTENT_LENGTH = 20 * 1024 * 1024    # 20 Mb limit
