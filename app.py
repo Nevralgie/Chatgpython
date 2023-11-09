38d6ce3bbcd4dc45d8a5d706b40fff82298439a8
Nevralgie <93102912+Nevralgie@users.noreply.github.com>
2023-11-09 13:06:11 +0100

    Update app.py

--- app.py
+++ app.py
@@ -8,11 +8,11 @@ app = Flask(__name__)
 
 # Replace with your actual HCP API token retrieval information
 hcpapi_token_url = "https://auth.hashicorp.com/oauth/token"
-hcpapi_client_id = ""
-hcpapi_client_secret = ""
+hcpapi_client_id = "ScF6ITDLLHe5bOYScpTfyBMCiG0XkPva"
+hcpapi_client_secret = "qdACtzLojKO9gYCzfc6oc3VBtshKSOoJEQVLUUk6W6gL9bhvz7uhbQP9BEfP7US-"
 
 vault_url = "https://api.hashicorp.cloud"
-vault_secret_path = ""
+vault_secret_path = "https://api.cloud.hashicorp.com/secrets/2023-06-13/organizations/92e300b2-dc96-41e1-af99-488fd920bf48/projects/3716cc7c-ed99-4279-a820-7dc4d78d7b54/apps/webapppy/open"
 
 def get_hcpapi_token(hcpapi_token_url, hcpapi_client_id, hcpapi_client_secret):
     headers = {'content-type': 'application/json'}
