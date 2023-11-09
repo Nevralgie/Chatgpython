d6236c1473eb8f4f50ac9d75151a788646c43195
Nevralgie <93102912+Nevralgie@users.noreply.github.com>
2023-11-09 17:57:05 +0100

    Update app.py

--- app.py
+++ app.py
@@ -7,9 +7,10 @@ from flask import Flask, request, render_template
 app = Flask(__name__)
 
 # Define at the module level
-connection_string = get_secret_from_vault()
-blob_service_client = BlobServiceClient.from_connection_string(connection_string)
+connection_string = None  # Initialize connection_string
+blob_service_client = None  # Initialize blob_service_client
 container_name = "test104"
+
 # Replace with your actual HCP API token retrieval information
 hcpapi_token_url = "https://auth.hashicorp.com/oauth/token"
 hcpapi_client_id = "ScF6ITDLLHe5bOYScpTfyBMCiG0XkPva"  # Replace with your actual client ID
@@ -17,23 +18,6 @@ hcpapi_client_secret = "qdACtzLojKO9gYCzfc6oc3VBtshKSOoJEQVLUUk6W6gL9bhvz7uhbQP9
 
 # Replace with your actual HashiCorp Vault secret path
 vault_secret_path = "https://api.cloud.hashicorp.com/secrets/2023-06-13/organizations/92e300b2-dc96-41e1-af99-488fd920bf48/projects/3716cc7c-ed99-4279-a820-7dc4d78d7b54/apps/webapppy/open"  # Replace with your secret path
-
-def get_hcpapi_token(hcpapi_token_url, hcpapi_client_id, hcpapi_client_secret):
-    headers = {'content-type': 'application/json'}
-    payload = {
-        "audience": "https://api.hashicorp.cloud",
-        "grant_type": "client_credentials",
-        "client_id": hcpapi_client_id,
-        "client_secret": hcpapi_client_secret
-    }
-
-    response = requests.post(hcpapi_token_url, headers=headers, data=json.dumps(payload))
-
-    if response.status_code == 200:
-        hcpapi_token = json.loads(response.text)["access_token"]
-        return hcpapi_token
-    else:
-        raise Exception("Failed to retrieve HCP API token")
 
 def get_secret_from_vault(vault_secret_path, hcpapi_token):
     headers = {"Authorization": f"Bearer {hcpapi_token}"}
@@ -52,8 +36,28 @@ def get_secret_from_vault(vault_secret_path, hcpapi_token):
     else:
         raise Exception(f"Failed to retrieve secret: {response.status_code} - {response.text}")
 
+def get_hcpapi_token(hcpapi_token_url, hcpapi_client_id, hcpapi_client_secret):
+    headers = {'content-type': 'application/json'}
+    payload = {
+        "audience": "https://api.hashicorp.cloud",
+        "grant_type": "client_credentials",
+        "client_id": hcpapi_client_id,
+        "client_secret": hcpapi_client_secret
+    }
+
+    response = requests.post(hcpapi_token_url, headers=headers, data=json.dumps(payload)
+
+    if response.status_code == 200:
+        hcpapi_token = json.loads(response.text)["access_token"]
+        return hcpapi_token
+    else:
+        raise Exception("Failed to retrieve HCP API token")
+
 @app.route('/')
 def index():
+    global connection_string  # Declare global connection_string
+    global blob_service_client  # Declare global blob_service_client
+
     # Retrieve the HCP API token
     hcpapi_token = get_hcpapi_token(hcpapi_token_url, hcpapi_client_id, hcpapi_client_secret)
 
@@ -64,10 +68,7 @@ def index():
         connection_string = secret_value
 
         if connection_string:
-            # Define your Azure Blob Storage account and container information
-            global blob_service_client 
             blob_service_client = BlobServiceClient.from_connection_string(connection_string)
-            container_name = container_name
 
             return render_template('index.html')
 
@@ -88,8 +89,7 @@ def upload_file():
 
         try:
             # Get a blob client
-            
-            blob_client = blob_service_client.get_blob_client(container=container_name, blob=file.filename)
+            blob_client = blob_service_client.get_blob_client(container=container_name, blob=filename)
 
             # Upload the file to Azure Blob Storage
             with file.stream as data:
@@ -102,7 +102,7 @@ def upload_file():
                 blob_name=filename,
                 account_key=blob_service_client.credential.account_key,
                 permission=BlobSasPermissions(read=True),
-                expiry=datetime.utcnow() + timedelta(hours=1)  # Adjust the expiration time by replacing the value of hours
+                expiry=datetime.utcnow() + timedelta(hours=1)
             )
 
             # Build the temporary download link
