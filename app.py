3ea82cf8c7c60fb11be289d6aa1ed6e786e176f4
Nevralgie <93102912+Nevralgie@users.noreply.github.com>
2023-11-09 16:05:54 +0100

    Update app.py

--- app.py
+++ app.py
@@ -11,7 +11,7 @@ hcpapi_token_url = "https://auth.hashicorp.com/oauth/token"
 hcpapi_client_id = "ScF6ITDLLHe5bOYScpTfyBMCiG0XkPva"  # Replace with your actual client ID
 hcpapi_client_secret = "qdACtzLojKO9gYCzfc6oc3VBtshKSOoJEQVLUUk6W6gL9bhvz7uhbQP9BEfP7US-"  # Replace with your actual client secret
 
-vault_url = "https://api.hashicorp.cloud"
+#vault_url = "https://api.hashicorp.cloud"
 vault_secret_path = "https://api.cloud.hashicorp.com/secrets/2023-06-13/organizations/92e300b2-dc96-41e1-af99-488fd920bf48/projects/3716cc7c-ed99-4279-a820-7dc4d78d7b54/apps/webapppy/open"  # Replace with your secret path
 
 def get_hcpapi_token(hcpapi_token_url, hcpapi_client_id, hcpapi_client_secret):
@@ -31,9 +31,9 @@ def get_hcpapi_token(hcpapi_token_url, hcpapi_client_id, hcpapi_client_secret):
     else:
         raise Exception("Failed to retrieve HCP API token")
 
-def get_secret_from_vault(vault_url, vault_secret_path, hcpapi_token):
+def get_secret_from_vault(vault_secret_path, hcpapi_token):
     headers = {"Authorization": f"Bearer {hcpapi_token}"}
-    response = requests.get(vault_url, headers=headers)
+    response = requests.get(vault_secret_path, headers=headers)
 
     if response.status_code == 200:
         secret_data = json.loads(response.text)["data"]
@@ -47,7 +47,7 @@ def index():
     hcpapi_token = get_hcpapi_token(hcpapi_token_url, hcpapi_client_id, hcpapi_client_secret)
 
     # Use the HCP API token to get the secret from HashiCorp Vault
-    secret_value = get_secret_from_vault(vault_url, vault_secret_path, hcpapi_token)
+    secret_value = get_secret_from_vault(vault_secret_path, hcpapi_token)
 
     if secret_value:
         connection_string = secret_value.get("connection_string")
