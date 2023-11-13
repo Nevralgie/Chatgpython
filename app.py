39d9428d7fdddfe614773a090c0b42e6bafd7d58
Nevralgie <93102912+Nevralgie@users.noreply.github.com>
2023-11-13 15:39:23 +0100

    Update app.py

--- app.py
+++ app.py
@@ -18,7 +18,7 @@ hcpapi_client_id = "cXvmplGRLcLqpf5LjoH3bhklzZr5sHuj"  # Replace with your actua
 hcpapi_client_secret = "nozcfrVSVb22woT_MYCZd4L75T1Ewg9b3MLt4Q0tVw-U7Yf7DomWK0obgKY6ENFE"  # Replace with your actual client secret
 
 # Replace with your actual HashiCorp Vault secret path
-vault_secret_path = "https://api.cloud.hashicorp.com/secrets/2023-06-13/organizations/92e300b2-dc96-41e1-af99-488fd920bf48/projects/3716cc7c-ed99-4279-a820-7dc4d78d7b54/apps/webapppy/open"  # Replace with your secret path
+vault_secret_path = "https://api.cloud.hashicorp.com/secrets/2023-06-13/organizations/92e300b2-dc96-41e1-af99-488fd920bf48/projects/3716cc7c-ed99-4279-a820-7dc4d78d7b54/apps/testgit/open"  # Replace with your secret path
 
 def get_secret_from_vault(vault_secret_path, hcpapi_token):
     headers = {"Authorization": f"Bearer {hcpapi_token}"}
